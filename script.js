0fb64d16442b9a4c57030fb34cb4e5efc55b45d3
Sarthak Deshpande <sarthakdeshpande1812@gmail.com>
2025-12-19 13:23:19 +0530

    Improved UX with auto-scroll to results

--- script.js
+++ script.js
@@ -6,21 +6,49 @@ document.getElementById("checkBtn").addEventListener("click", () => {
     return;
   }
 
-  // Mock data
+  // MOCK DATA (replace later with API)
   const aqi = 132;
   const greenScore = 50;
+  let status = "Good";
+  if (greenScore < 60) status = "Moderate";
+  if (greenScore < 40) status = "Poor";
+
   const suggestions = [
     "Avoid peak traffic hours",
     "Use public transport",
     "Wear a mask outdoors"
   ];
 
+  // Color logic
+  let color = "#2ecc71";
+  if (greenScore < 60) color = "#f1c40f";
+  if (greenScore < 40) color = "#e74c3c";
+  document.documentElement.style.setProperty("--scoreColor", color);
+
   document.getElementById("resultSection").innerHTML = `
-    <h3>📍 ${city}</h3>
-    <p><strong>AQI:</strong> ${aqi}</p>
-    <p><strong>GreenScore:</strong> ${greenScore}</p>
-    <ul>
-      ${suggestions.map(s => `<li>${s}</li>`).join("")}
-    </ul>
+    <div class="card fade-in">
+      <h3>📍 ${city}</h3>
+
+      <div class="score">
+        <span>AQI</span>
+        <strong>${aqi}</strong>
+      </div>
+
+     <div class="greenscore">
+       <span>GreenScore</span>
+       <h1>${greenScore}</h1>
+      <p class="status">${status}</p>
+      </div>
+
+      <h4>What you can do today 🌱</h4>
+      <ul>
+        ${suggestions.map(s => `<li>✔ ${s}</li>`).join("")}
+      </ul>
+    </div>
   `;
+
+    document.getElementById("resultSection").scrollIntoView({
+     behavior: "smooth"
+  });
+
 });
